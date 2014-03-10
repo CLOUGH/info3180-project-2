41dea8956246539712b17d057ed74093f3fdd2ed
Warren Clough <clough.warren@gmail.com>
2014-03-10 02:14:18 -0500

    Updated app to final version

--- static/cards.js
+++ static/cards.js
@@ -1,10 +1,15 @@
-var cards_seleceted= new Array();
+var cards_selected= new Array();
+var cards_selected_index= new Array();
 var found_matches = new Array();
 
 var deck_size = 16;
 var max_card_flips = 2;
 var card_flips =0;
 var MAX_FLIP = 26;
+var deck;
+var fname;
+var lname;
+
 
 var createDeck = function() {
 // based on code from http://www.brainjar.com/js/cards/default2.asp
@@ -33,10 +38,11 @@ var createDeck = function() {
   //console.log(cards.length);
   return cards;
 }
-var showCards = function(cardJSON) {
+var showCards = function(cardJSON,deck_index) {
   txt = cardJSON.rank /*+ cardJSON.suite*/;    
 
   card = document.createElement("div");
+  card.id = deck_index;
   var card_content = document.createElement('p');
   var card_cover = document.createElement('div');
 
@@ -70,9 +76,11 @@ var showCards = function(cardJSON) {
   document.querySelector(".sideBox").appendChild(card);
 }
 
-var showDeck = function(deck){
+//TODO: should change name to generate-random deck
+var generateRandDeck = function(deck){
 
     var card_picked = new Array();
+    var new_deck =  new Array();
 
     //generate random deck picks
     var random_card_count = 0;
@@ -91,20 +99,27 @@ var showDeck = function(deck){
 
     for (var i = 0; i <deck_size; i++) {
       var random_pick = Math.floor((Math.random()*card_picked.length));
-      showCards(deck[card_picked[random_pick]]);
+      //create a new array with the cards
+      new_deck.push(deck[card_picked[random_pick]]);
+      //showCards(deck[card_picked[random_pick]]);
       card_picked.splice(random_pick,1);
       
     };
-    /*for (var i = 0; i < deck.length; ++i) {
-      showCards(deck[i]);
-    }*/
+
+    return new_deck;
+}
+
+function showDeck(deck){
+  for(var i=0;i<deck.length;i++){
+      showCards(deck[i],i);
+  }
 }
 function memory_game (selected_card) {
 
   if(card_flips <= MAX_FLIP-1)
   {
     // if the card was not seleceted before then add it to selected list
-    if(selected_card.lastElementChild.hidden == false)
+    if($(selected_card.lastElementChild).hasClass('hidden')== false)
     {
       var match_index;
       flip_card(selected_card);
@@ -112,13 +127,14 @@ function memory_game (selected_card) {
       card_flips += 1;
 
       //from the selected cards  check if the card matched
-      if((match_index =isMatched(selected_card, cards_seleceted))!=-1)
+      if((match_index =isMatched(selected_card, cards_selected))!=-1)
       {
         //push matched cards to the found_match array and remove the selected card from the selected card_array
-        found_matches.push(cards_seleceted[match_index]);
-        found_matches.push(selected_card)
+        found_matches.push(cards_selected[match_index].id);
+        found_matches.push(selected_card.id)
 
-        cards_seleceted.splice(match_index,1);
+        cards_selected.splice(match_index,1);
+        cards_selected_index.splice(match_index,1);
 
         //play matched audio   
         $("#matched-audio")[0].load();
@@ -127,28 +143,35 @@ function memory_game (selected_card) {
       else
       {
         //if the max amount of card flips have been reached then hide all the unmatched selected cards
-        if(cards_seleceted.length>=max_card_flips)
+        if(cards_selected.length>=max_card_flips)
         {
-          var cards_selected_length = cards_seleceted.length;
+          cards_selected_length = cards_selected.length;
           for (var i=0; i<cards_selected_length; i++)
           {
-            flip_card(cards_seleceted[0]);
-            cards_seleceted.splice(0,1);        
+            flip_card(cards_selected[0]);
+            cards_selected.splice(0,1);
+            cards_selected_index.splice(match_index,1);        
           }
         }
 
-        cards_seleceted.push(selected_card);
+        cards_selected.push(selected_card);
+        var test = selected_card.id;
+        cards_selected_index.push(test);
       }   
     } 
 
     if(found_matches.length==deck_size)
     {
-      alert("You have won!");
+      $("#victory-song")[0].load();
+      $("#victory-song")[0].play();
+      $('#win-modal').modal('setting', 'closable', false).modal('show');
+      
     }
   }
   else
   {
-    alert("Sorry you have reached the max flips of "+MAX_FLIP+"!");
+    $("#max-flip").text(MAX_FLIP);
+    $('#lost-modal').modal('setting', 'closable', false).modal('show');
   }
 
 }
@@ -157,15 +180,26 @@ function updatePlayerInfo()
   //update number of flips
   $("#card-flips").text(card_flips);
   $("#max-flips").text(MAX_FLIP);
-  localStorage.setItem('card-flips', JSON.stringify(card_flips));
 
-  //update player name
-  var fname = localStorage.getItem("player_fname")
-  var lname = localStorage.getItem("player_lname")
   $('#player_name').text(fname+" "+lname);
 }
-function savePlayerInfo()
+function playAgain()
 {
+  localStorage.removeItem('player_fname');
+  localStorage.removeItem('player_lname');          
+  localStorage.removeItem('card-deck');
+  localStorage.removeItem('found-cards');
+  localStorage.removeItem('card-flips');
+  localStorage.removeItem('selected-cards');
+  $('#card-box').empty();
+  
+  found_matches = new Array();  
+  cards_selected_index = new Array();
+  cards_selected = new Array();
+  deck = generateRandDeck(createDeck());
+  showDeck(deck);       
+  card_flips = 0;       
+  updatePlayerInfo();
 
 }
 
@@ -180,9 +214,18 @@ function  isMatched(needle, haystack)
   return -1;
 }
 
+function turnFlipedCards(found_cards){
+  for(var i=0;i<found_cards.length;i++)
+  {
+    var card_id = found_cards[i];
+    var card = document.getElementById(card_id);
 
+    flip_card(card);
+  }
+}
 function flip_card (selected_card) {
   $(selected_card.lastElementChild).transition('horizontal flip', '500ms');
+  $("#card-flick")[0].load();
+  $("#card-flick")[0].play();
 }
-var deck = createDeck();
-showDeck(deck);
+
